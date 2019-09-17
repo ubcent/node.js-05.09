e68cb19e25dfe3a7f0fd710dc15655d079c8ed83
Vladimir <v.k.lazarev@tinkoff.ru>
2019-09-17 15:08:45 +0300

    fix after comments

--- v.lazarev/task2_1.js
+++ v.lazarev/task2_1.js
@@ -2,20 +2,19 @@ const readline = require('readline');
 const argv = require('minimist')(process.argv.slice(2));
 const fs = require('fs');
 
-var logname = 'log.txt'
+let logname = 'log.txt'
 
 if (argv.l){
-  console.log(argv);
   logname = argv.l;
 }
 
-var rl = readline.createInterface({
+let rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-console.log("Input 1 or 2 to guess. Input stop to quit");
-fs.appendFile (logname, "g\n", () => {
+console.log('Input 1 or 2 to guess. Input stop to quit');
+fs.appendFile (logname, 'g\n', () => {
 
 });
 
@@ -25,13 +24,9 @@ rl.on('line', (inpt) => {
     rl.close();
   } else if (inpt === coin){
     console.log('You won :)');
-    fs.appendFile (logname, '1\n', () => {
-
-    });
+    fs.appendFileSync (logname, '1\n');
   } else {
     console.log('You lost :(');
-    fs.appendFile (logname, '0\n', () => {
-
-    });
+    fs.appendFileSync (logname, '0\n');
   }
 });
--- v.lazarev/task2_2.js
+++ v.lazarev/task2_2.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const argv = require('minimist')(process.argv.slice(2));
 
-var logname = 'log.txt'
+let logname = 'log.txt'
 
 if (argv.l){
   console.log(argv);
@@ -9,45 +9,64 @@ if (argv.l){
 }
 
 fs.readFile(logname, (err, data) => {
-  let games = 0, rounds = 0, won = 0, lost = 0, winrate = 0, winstreak = 0, losestreak = 0, streak = 0, streak_log = 0;
+  let games = 0;
+  let rounds = 0;
+  let won = 0;
+  let lost = 0;
+  let winrate = 0;
+  let winstreak = 0;
+  let losestreak = 0;
+  let streak = 0;
+  let streak_log = 0;
+
   if (err){
     throw err;
   }
+
   data = data.toString();
-  for(let i = 0; i < data.length; i++){
+
+  for(let i = 0; i < data.length; i++){ 
     if (data[i] === 'g'){
       games++;
       streak = 0;
     }
+
     if (data[i] === '1'){
       if (!streak) {
         streak = 0;
         streak_log = 1;
       }
+
       rounds++;
       won++;
       streak++;
+      
       if (streak > winstreak){
         winstreak = streak;
       }
     }
+    
     if (data[i] === '0'){
       if (streak) {
         streak = 0;
         streak_log = 0;
       }
+      
       rounds++;
       lost++;
       streak++;
+      
       if (streak > losestreak){
         losestreak = streak;
       }
     }
   }
+
   if (!lost) {
     winrate = won/1;
   } else {
     winrate = won/lost;
   }
-  console.log("Games: ", games, " Rounds: ", rounds, " Won: ", won, " Lost: ", lost, " Win/lose: ", winrate, " Longest winning streak: ", winstreak, " Longest losing streak: ", losestreak);
+
+  console.log('Games: ', games, ' Rounds: ', rounds, ' Won: ', won, ' Lost: ', lost, '  Win/lose: ', winrate, ' Longest winning streak: ', winstreak, ' Longest losing streak: ', losestreak);
 });
